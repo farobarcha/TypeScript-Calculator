acc7beb18bea55ca067a265d52694e0ee2b9c6ea
Farhan Hussain <farhanbarcha@hotmail.com>
2024-03-29 18:29:56 +0500

    feat: initial files

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # TypeScript-Calculator
 A simple calculator build in TypeScript using Inquirer
+
+## Instructions
+
+1. Clone or download this repository
+2. Open the folder in your terminal
+3. Run `npm install` to install the dependencies
+4. Run `node index` to start the calculator
+
+## Features
+
+* Simple arithmetic operations (+, -, \*, /)
+* Addition
+* Subtraction
+* Multiplication
+* Division
+* Square Root
+* Percentage
---
+++ index.ts
@@ -0,0 +1,89 @@
+import inquirer from 'inquirer';
+
+let result = 0;
+let resultMessage = '';
+
+const calculator = await inquirer.prompt([
+    {
+        type: 'list',
+        name: 'operations',
+        message: 'Select Operation to perform: ',
+        choices: ['Addition', 'Subtraction', 'Multiplication', 'Division', 'Square Root', 'Percentage'],
+    },
+    {
+        type: 'input',
+        name: 'operand1',
+        message: "Enter a Number: ",
+        validate(value: number) {
+            if (isNaN(value)) {
+                return 'Please enter a valid number';
+            }
+            return true;
+        },
+    },
+    {
+        type: 'input',
+        name: 'operand2',
+        message: "Enter a second Number: ",
+        when(choices) {
+            return ['Addition', 'Subtraction', 'Multiplication', 'Division'].includes(choices.operations);
+        },
+        validate(value: number) {
+            if (isNaN(value)) {
+                return 'Please enter a valid number';
+            }
+            return true;
+        },
+    },
+    {
+        type: 'input',
+        name: 'percentage',
+        message: "Enter percentage(%): ",
+        when(choices) {
+            return choices.operations == 'Percentage';
+        },
+        validate(value: number) {
+            if (isNaN(value)) {
+                return 'Please enter a valid value';
+            }
+            return true;
+        },
+    },
+]);
+
+switch (calculator.operations) {
+    case 'Addition':
+        result = parseFloat(calculator.operand1) + parseFloat(calculator.operand2);
+        resultMessage = 'Operation: ' + calculator.operand1 + ' + ' + calculator.operand2;
+        break;
+    case 'Subtraction':
+        result = parseFloat(calculator.operand1) - parseFloat(calculator.operand2);
+        resultMessage = 'Operation: ' + calculator.operand1 + ' - ' + calculator.operand2;
+        break;
+    case 'Multiplication':
+        result = parseFloat(calculator.operand1) * parseFloat(calculator.operand2);
+        resultMessage = 'Operation: ' + calculator.operand1 + ' X ' + calculator.operand2;
+        break;
+    case 'Division':
+        result = parseFloat(calculator.operand1) / parseFloat(calculator.operand2);
+        resultMessage = 'Operation: ' + calculator.operand1 + ' / ' + calculator.operand2;
+        break;
+    case 'Square Root':
+        result = parseFloat(calculator.operand1) * parseFloat(calculator.operand1);
+        resultMessage = 'Square root of: ' + calculator.operand1;
+        break;
+    case 'Percentage':
+        result = (parseFloat(calculator.percentage) / 100) * parseFloat(calculator.operand1);
+        resultMessage = calculator.percentage + ' % of ' + calculator.operand1;
+        break;
+    default:
+        console.log('Please select an valid operation.')
+        break;
+}
+
+
+console.log('\n==================================');
+console.log(resultMessage);
+console.log('Result: ' + result);
+console.log('==================================');
+
