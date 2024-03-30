865bca6b5997d1964129a16de4d81d9b3e4dcadc
Farhan Hussain <farhanbarcha@hotmail.com>
2024-03-30 14:45:01 +0500

    fix: add chalk

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 import inquirer from 'inquirer';
+import chalk from 'chalk';
 let result = 0;
 let resultMessage = '';
 const calculator = await inquirer.prompt([
@@ -7,14 +8,14 @@ const calculator = await inquirer.prompt([
         type: 'list',
         name: 'operations',
         message: 'Select Operation to perform: ',
-        choices: ['Addition', 'Subtraction', 'Multiplication', 'Division', 'Square Root', 'Percentage'],
+        choices: ['Addition', 'Subtraction', 'Multiplication', 'Division', 'Square', 'Percentage'],
     },
     {
         type: 'input',
         name: 'operand1',
         message: "Enter a Number: ",
         validate(value) {
-            if (isNaN(value)) {
+            if (!value || isNaN(value)) {
                 return 'Please enter a valid number';
             }
             return true;
@@ -28,7 +29,7 @@ const calculator = await inquirer.prompt([
             return ['Addition', 'Subtraction', 'Multiplication', 'Division'].includes(choices.operations);
         },
         validate(value) {
-            if (isNaN(value)) {
+            if (!value || isNaN(value)) {
                 return 'Please enter a valid number';
             }
             return true;
@@ -66,9 +67,9 @@ switch (calculator.operations) {
         result = parseFloat(calculator.operand1) / parseFloat(calculator.operand2);
         resultMessage = 'Operation: ' + calculator.operand1 + ' / ' + calculator.operand2;
         break;
-    case 'Square Root':
+    case 'Square':
         result = parseFloat(calculator.operand1) * parseFloat(calculator.operand1);
-        resultMessage = 'Square root of: ' + calculator.operand1;
+        resultMessage = 'Square of: ' + calculator.operand1;
         break;
     case 'Percentage':
         result = (parseFloat(calculator.percentage) / 100) * parseFloat(calculator.operand1);
@@ -78,7 +79,7 @@ switch (calculator.operations) {
         console.log('Please select an valid operation.');
         break;
 }
-console.log('\n==================================');
-console.log(resultMessage);
-console.log('Result: ' + result);
-console.log('==================================');
+console.log(chalk.yellow('\n=================================='));
+console.log(chalk.blue(resultMessage));
+console.log(chalk.bold('Result: ' + chalk.green(result)));
+console.log(chalk.yellow('=================================='));
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 import inquirer from 'inquirer';
+import chalk from 'chalk';
 
 let result = 0;
 let resultMessage = '';
@@ -9,14 +10,14 @@ const calculator = await inquirer.prompt([
         type: 'list',
         name: 'operations',
         message: 'Select Operation to perform: ',
-        choices: ['Addition', 'Subtraction', 'Multiplication', 'Division', 'Square Root', 'Percentage'],
+        choices: ['Addition', 'Subtraction', 'Multiplication', 'Division', 'Square', 'Percentage'],
     },
     {
         type: 'input',
         name: 'operand1',
         message: "Enter a Number: ",
         validate(value: number) {
-            if (isNaN(value)) {
+            if (!value || isNaN(value)) {
                 return 'Please enter a valid number';
             }
             return true;
@@ -30,7 +31,7 @@ const calculator = await inquirer.prompt([
             return ['Addition', 'Subtraction', 'Multiplication', 'Division'].includes(choices.operations);
         },
         validate(value: number) {
-            if (isNaN(value)) {
+            if (!value || isNaN(value)) {
                 return 'Please enter a valid number';
             }
             return true;
@@ -69,9 +70,9 @@ switch (calculator.operations) {
         result = parseFloat(calculator.operand1) / parseFloat(calculator.operand2);
         resultMessage = 'Operation: ' + calculator.operand1 + ' / ' + calculator.operand2;
         break;
-    case 'Square Root':
+    case 'Square':
         result = parseFloat(calculator.operand1) * parseFloat(calculator.operand1);
-        resultMessage = 'Square root of: ' + calculator.operand1;
+        resultMessage = 'Square of: ' + calculator.operand1;
         break;
     case 'Percentage':
         result = (parseFloat(calculator.percentage) / 100) * parseFloat(calculator.operand1);
@@ -83,8 +84,8 @@ switch (calculator.operations) {
 }
 
 
-console.log('\n==================================');
-console.log(resultMessage);
-console.log('Result: ' + result);
-console.log('==================================');
+console.log(chalk.yellow('\n=================================='));
+console.log(chalk.blue(resultMessage));
+console.log(chalk.bold('Result: ' + chalk.green(result)));
+console.log(chalk.yellow('=================================='));
 
